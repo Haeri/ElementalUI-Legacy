8af0c44b50bf5a6ccf3b297b42b200afd2a148f1
Haeri <luka.l@gmx.ch>
2020-09-17 01:59:55 +0200

    Initial Commit

---
+++ tools/generate_project_win.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Store caller path
+set old_path=%cd%
+set err=0 
+cd "%~dp0"
+::-----------------------
+
+
+
+
+cd ..
+rmdir /S /Q "build"
+mkdir "build"
+cd build
+
+cmake .. -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="../extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
+set /a "err=%err%+%errorlevel%"
+
+
+
+
+::-----------------------
+:: Restore to caller path
+cd "%old_path%"
+if not "%1" == "-s" pause
+exit /b %err%
