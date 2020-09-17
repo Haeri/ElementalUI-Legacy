870021250b7d069875e7e36c9296247d4d907ab0
Haeri <luka.l@gmx.ch>
2020-09-17 23:30:57 +0200

    overlay port

---
+++ extern/custom-ports/elemd/portfile.cmake
@@ -0,0 +1,30 @@
+#vcpkg_from_github(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    REPO Haeri/ElementalDraw
+#    REF v0.0.1
+#    SHA512 f0d15983bd6d4c9a7cde127ddc66050aa88f98a86d21e310aed78c30e11bf0df20a86869874bd14c8beeaae4ffb347071de180c8c35ae557dab5d5af0fa74663
+#    HEAD_REF master
+#)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/Haeri/ElementalDraw/archive/master.zip"
+    SHA512 3dc42d5caf17aab16f5c154080f020d5aed761e22db4c5f6506917f6bfd2bf8becfb40af919042bd4ce1077d5de74aa666f5edfba7f275efba78e8893c115148
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+#    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/elemd)
+
+# # Moves all .cmake files from /debug/share/elemd/ to /share/elemd/
+# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
+# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/elemd)
