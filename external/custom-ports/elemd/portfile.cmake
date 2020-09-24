89cfc2ec74acf08bcfae2e996af213433491afc2
Haeri <Haeri@users.noreply.github.com>
2020-09-24 02:24:36 +0200

    Maybe

--- external/custom-ports/elemd/portfile.cmake
+++ external/custom-ports/elemd/portfile.cmake
@@ -7,10 +7,9 @@
 #)
 
 vcpkg_download_distfile(ARCHIVE
-#    URLS "https://github.com/Haeri/ElementalDraw/archive/master.zip"
-	URLS https://github.com/Haeri/ElementalDraw/archive/3d2b2301dac096b58ba35643ebba5f3f83c61618.zip
+	URLS https://github.com/Haeri/ElementalDraw/archive/c6391e9483a13eac04a25e31ffaf98b31b95d024.zip
 	FILENAME elemd001.zip
-    SHA512 99d3a3fac5891bce52b063aee4468f4e264841d0cd67a1ac9a3f1fd32f089698919a115eec607b2a5c9cd137ce89277cf27e5d22d01263d973e8d81535d6a971
+    SHA512 155480a20dfb4a09c71d343408c404abc43027080eff15ffeb7dba9f352f110252b8952473fec93023b5cddd861c069c96648444bd44f942c1d164e7e00c5802
 )
 
 vcpkg_extract_source_archive_ex(
@@ -24,9 +23,11 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/elemd RENAME copyright)
 
 # # Moves all .cmake files from /debug/share/elemd/ to /share/elemd/
 # # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
-# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/elemd)
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
